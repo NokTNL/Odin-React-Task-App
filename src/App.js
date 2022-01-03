232d5acd01b5b1118039fb89cc510c0da5aa6d88
NokTNL <noklamtsz@gmail.com>
2022-01-03 21:27:32 +0000

    Use non-mutating methods to update states
    Tried avoiding mutating methods such as Array.splice() and assigning values to
    objects nested inside arrays directly. Used Array.filter() and
    Array.map() instead, respectively

--- src/App.js
+++ src/App.js
@@ -10,23 +10,30 @@ const TEMPLATE_TASKS = [
 
 function App() {
   const [tasks, setTasks] = useState(TEMPLATE_TASKS);
+  console.log(tasks);
 
   const onDeleteTask = (deletingId) => {
-    setTasks((prevTasks) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== deletingId));
+    /* Alternative: using splice() (Usually not preferred) 
       const spliceIndex = prevTasks.findIndex((task) => task.id === deletingId);
       const newTasks = [...prevTasks];
       newTasks.splice(spliceIndex, 1);
-      return newTasks;
-    });
+      return newTasks; 
+    }); */
   };
 
   const onResubmit = ({ modifiedTaskContent, editedId }) => {
     setTasks((prevTasks) => {
-      const editedIndex = prevTasks.findIndex((task) => task.id === editedId);
+      return prevTasks.map((task) => {
+        if (task.id === editedId)
+          return { ...task, content: modifiedTaskContent };
+        else return task;
+      });
+      // This one should NOT be used, although it works. (...) only does shallow copy.
+      /* const editedIndex = prevTasks.findIndex((task) => task.id === editedId);
       const newTasks = [...prevTasks];
-      newTasks[editedIndex].content = modifiedTaskContent;
-      console.log(newTasks);
-      return newTasks;
+      newTasks[editedIndex].content = modifiedTaskContent; // This is mutating the object inside the state variable
+      return newTasks; */
     });
   };
 
